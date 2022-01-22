5f6c4c6bd7d7fe62075067b4b3eba52edfae6ac5
jamiep9rk <jamiecareer96@gmail.com>
2022-01-22 17:32:28 +0900

    ADD: 로또의 최고 순위와 최저 순위 풀이 수정

--- src/programmers/Level1/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위 answer.js
+++ src/programmers/Level1/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위 answer.js
@@ -6,20 +6,36 @@ function solution(lottos, win_nums) {
     // lottos의 모든 요소가 0일 경우 [1,6]을 리턴한다
     // 반복문이 다 돌면 최소의 순위에서 0의 갯수를 빼서 최고의 순위를 계산해서 리턴한다
 
-    let answer = [0, 7];
-    let lottosSort = lottos.filter((el) => el !== 0).sort((a, b) => a - b);
-    let percent = lottos.length - lottosSort.length;
-    win_nums = win_nums.sort((a, b) => a - b);
+    // 테스트 하나 통과 안되는 코드
+    //     let answer = [0, 7];
+    //     let lottosSort = lottos.filter((el) => el !== 0).sort((a, b) => a - b);
+    //     let percent = lottos.length - lottosSort.length;
+    //     win_nums = win_nums.sort((a, b) => a - b);
 
-    for (let i = 0; i < lottosSort.length; i++) {
-        if (win_nums.includes(lottosSort[i])) {
-            answer[1]--;
-        }
-    }
-    if (percent === lottos.length) {
-        answer = [1, 6];
-    } else {
-        answer[0] = answer[1] - percent;
-    }
-    return answer;
+    //     for (let i = 0; i < lottosSort.length; i++) {
+    //         if (win_nums.includes(lottosSort[i])) {
+    //             answer[1]--;
+    //         }
+    //     }
+    //     if (percent === lottos.length) {
+    //         answer = [1, 6];
+    //     } else {
+    //         answer[0] = answer[1] - percent;
+    //     }
+    //     return answer;
+    // }
+
+    const result = [];
+
+    let correctCnt = lottos.filter((num) => win_nums.includes(num)).length;
+    let zeroCnt = lottos.filter((num) => num === 0).length;
+
+    // 당첨 번호와 같은 번호의 개수가 0~1개라면 6위, 아니라면 7-correctCnt로 최저 순위 반환
+    let minRank = 7 - correctCnt >= 6 ? 6 : 7 - correctCnt;
+    // 0의 개수가 6개라면 최고 순위 1위 가능, 6개 아니라면 0의 개수 만큼 숫자를 빼서 순위 올림
+    let maxRank = zeroCnt === 6 ? 1 : minRank - zeroCnt;
+
+    result.push(maxRank, minRank);
+
+    return result;
 }
