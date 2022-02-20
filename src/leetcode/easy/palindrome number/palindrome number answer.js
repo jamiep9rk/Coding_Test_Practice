840a4bea71fda7ee2a0abb5d6209f1d8661b85da
jamiep9rk <jamiecareer96@gmail.com>
2022-02-20 22:30:06 +0900

    ADD: palindrome number 풀이

--- src/leetcode/easy/palindrome number/palindrome number answer.js
+++ src/leetcode/easy/palindrome number/palindrome number answer.js
@@ -1,18 +1,13 @@
 var isPalindrome = function (x) {
-    let string = "";
-    let half = 0;
-    if (x < 0) {
-        string = String(Math.abs(x));
-        half = Math.floor(string.length / 2);
-    } else {
-        string = String(x);
-        half = Math.floor(string.length / 2);
-    }
+    // 숫자를 문자열로 바꿔서 선언한다
+    // 반복문을 문자열 길이의 반의 인덱스까지 돌린다
+    // 앞과 뒤의 인덱스의 문자열을 비교하면서 같으면 true, 다르면 false를 리턴한다
+
+    let string = String(x);
+    let half = Math.floor(string.length / 2);
 
     for (let i = 0; i < half; i++) {
-        if (string[i] === string[string.length - 1 - i]) {
-            continue;
-        } else if (string[i] === string[string.length - 1 - i]) {
+        if (string[i] !== string[string.length - 1 - i]) {
             return false;
         }
     }
